5982a2ffcc2604aed0bf09147c77566a7561b079
Kodili118 <nwejedumebi0@gmail.com>
2024-12-11 16:25:36 +0100

    (practice_2)

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+fn main(){
+	let mut file = std::fs::File::open("welcome_message.txt").unwrap();
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).unwrap();
+	print!("{}", contents);
 }
---
+++ week-9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\nweje\Documents\c.nwejeCSC101\week-9\practice_2\target\debug\practice_2.exe: C:\Users\nweje\Documents\c.nwejeCSC101\week-9\practice_2\src\main.rs
