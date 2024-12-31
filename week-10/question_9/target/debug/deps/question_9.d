38685f76220f43369cbcd4d1df5de5c3ab500c9e
Kodili118 <nwejedumebi0@gmail.com>
2024-12-31 15:52:48 +0100

    (question_9)

---
+++ week-10/question_9/target/debug/deps/question_9.d
@@ -0,0 +1,5 @@
+C:\Users\nweje\Documents\c.nwejeCSC101\week-10\question_9\target\debug\deps\question_9.exe: src/main.rs
+
+C:\Users\nweje\Documents\c.nwejeCSC101\week-10\question_9\target\debug\deps\question_9.d: src/main.rs
+
+src/main.rs:
