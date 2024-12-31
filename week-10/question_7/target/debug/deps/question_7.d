5bd7f15a991ec4a889d9c6260b4245b6f2ee1aeb
Kodili118 <nwejedumebi0@gmail.com>
2024-12-31 15:28:18 +0100

    (question_7)

---
+++ week-10/question_7/target/debug/deps/question_7.d
@@ -0,0 +1,5 @@
+C:\Users\nweje\Documents\c.nwejeCSC101\week-10\question_7\target\debug\deps\question_7.exe: src/main.rs
+
+C:\Users\nweje\Documents\c.nwejeCSC101\week-10\question_7\target\debug\deps\question_7.d: src/main.rs
+
+src/main.rs:
---
+++ week-10/question_7/target/debug/question_7.d
@@ -0,0 +1,1 @@
+C:\Users\nweje\Documents\c.nwejeCSC101\week-10\question_7\target\debug\question_7.exe: C:\Users\nweje\Documents\c.nwejeCSC101\week-10\question_7\src\main.rs
